e654dce7c7a048af508c614ae79aa701e884e2a7
Caleb Free <cdfree@unc.edu>
2024-11-14 17:28:36 -0500

    Reverted Link

--- frontend/src/components/classifier-comps/camera-utils.tsx
+++ frontend/src/components/classifier-comps/camera-utils.tsx
@@ -33,7 +33,7 @@ const Camera = ({setImageData}: {setImageData: (data: BookData) => void}) => {
         const imageSrc: String | null | undefined = capture();
         if (imageSrc) {
             console.log(imageSrc);
-            fetch(`${process.env.BACK_END_URL}`, {
+            fetch('https://booklyai.onrender.com/classification/classify-book', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
